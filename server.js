57894c75b273519ccda206c54a6f1577248cf4dc
ahmednishaz <53562427+ahmednishaz@users.noreply.github.com>
2020-11-03 01:43:18 +0500

    Chapter-5

--- server.js
+++ server.js
@@ -1,14 +1,45 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
 
-//load env vars
+// load env vars
 dotenv.config({path: './config/config.env'});
+
+// Connect to database
+connectDB();
+
+// Route files
+const countries = require('./routes/countries'); 
+const connectDb = require('./config/db');
 
 const app = express();
 
+// Body Parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/countries', countries);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT, 
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`.yellow.bold)
   );
+
+// Handled unhandled promise rejections
+process.on('unhandledRejection', (err, promise)=>{
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(()=> process.exit(1));
+})
